2c4a4c44e00d8847f9c4a4c3c2a3df455d97ccf2
Arun Kumar <arun94255@gmail.com>
2018-10-02 16:35:31 +0530

    Update selectquery.sql

--- DBMS_LAB/selectquery.sql
+++ DBMS_LAB/selectquery.sql
@@ -35,3 +35,55 @@ select ((select count(*) from station)-(select count(distinct city) from station
 
 select CITY, length(CITY)from station order by length(CITY), city limit 1;
 select CITY, length(CITY)from station order by length(CITY) desc, city limit 1; `
+                                        
+                                        
+                                        /* Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.*/
+                                        
+                                        select distinct city from station where city like 'a%';
+select distinct city from station where city like 'i%';
+select distinct city from station where city like 'u%' ;
+select distinct city from station where city like 'e%';
+select distinct city from station where city like 'o%';
+                                        
+          /* Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.  */
+                                        
+                                        select distinct city from station where city like '%a';
+select distinct city from station where city like '%i';
+select distinct city from station where city like '%u' ;
+select distinct city from station where city like '%e';
+select distinct city from station where city like '%o';
+                                        
+                    /* Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates. */
+                                        
+
+   select distinct city from station where SUBSTRING(city,1,1) in ('A','E','I','O','U') and 
+    SUBSTRING(city,-1,1) in ('A','E','I','O','U');
+                                        
+     /* Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.*/
+     select distinct city from station where SUBSTRING(city,1,1) not in ('A','E','I','O','U') ;
+                                        
+ /*Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates. */
+                                        
+                                        
+                                        
+  
+  /* Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.*/
+  select distinct city from station where SUBSTRING(city,-1,1) not in ('A','E','I','O','U') ;
+                                        
+                                        
+   
+/* Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.*/
+  select distinct city from station where SUBSTRING(city,1,1) not in ('A','E','I','O','U') or 
+    SUBSTRING(city,-1,1) not in ('A','E','I','O','U');
+                                        
+                                        
+ /*Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.*/
+  select distinct city from station where SUBSTRING(city,1,1) not in ('A','E','I','O','U') and
+    SUBSTRING(city,-1,1) not in ('A','E','I','O','U');
+                                        
+                                        
+ /* 
+
+
+                                        
+                                        
