d89ba86aa2ae23603ca3ee9698f571059dec05b1
Arun Kumar <arun94255@gmail.com>
2018-10-04 16:50:29 +0530

    Update selectquery.sql

--- DBMS_LAB/selectquery.sql
+++ DBMS_LAB/selectquery.sql
@@ -82,7 +82,12 @@ select distinct city from station where city like '%o';
     SUBSTRING(city,-1,1) not in ('A','E','I','O','U');
                                         
                                         
- /* 
+ /* A median is defined as a number separating the higher half of a data set from the lower half. Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to  decimal places.
+*/
+                                        
+Select round(S.LAT_N,4) mediam from station S where (select count(Lat_N) from station where Lat_N < S.LAT_N ) = (select count(Lat_N) from station where Lat_N > S.LAT_N);
+                                        
+                                        
 
 
                                         
